7e6dae4714761a7e6db57c6b92f9b48d3a545c89
Daria V. Khrustaleva <snowstorm1@yandex.ru>
2025-01-09 01:48:24 +0300

    Initial agent implementation.

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,84 @@
 package main
 
-func main() {}
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"time"
+
+	"github.com/dvkhr/metrix.git/internal/metric"
+)
+
+type MetricType string
+
+const (
+	GaugeMetric   = MetricType("gauge")
+	CounterMetric = MetricType("counter")
+)
+
+func main() {
+
+	var mStor metric.MemStorage
+	mStor.NewMemStorage()
+
+	iteration := 0
+
+	cl := &http.Client{}
+
+	for {
+		iteration++
+		mStor.CollectMetrics()
+
+		if iteration >= 5 {
+			fmt.Printf("+++Send metrics to server+++\n")
+
+			for mtrxName, mtrxVal := range mStor.AllCounterMetrics() {
+				err := callURL(cl, buildMetricURL("counter", mtrxName, fmt.Sprintf("%v", mtrxVal)))
+				if err != nil {
+					continue
+				}
+			}
+
+			for mtrxName, mtrxVal := range mStor.AllGaugeMetrics() {
+				err := callURL(cl, buildMetricURL("gauge", mtrxName, fmt.Sprintf("%v", mtrxVal)))
+				if err != nil {
+					continue
+				}
+			}
+
+			iteration = 0
+		}
+		time.Sleep(2 * time.Second)
+	}
+}
+
+func buildMetricURL(mType string, mName string, mValue string) string {
+	serverUrl := &url.URL{
+		Scheme: "http",
+		Host:   "localhost:8080",
+		Path:   fmt.Sprintf("update/%s/%s/%s", mType, mName, mValue),
+	}
+
+	return serverUrl.String()
+}
+
+func callURL(cl *http.Client, url string) error {
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "text/plain")
+
+	res, err := cl.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return errors.New("bad http status")
+	}
+
+	return nil
+}
--- internal/metric/mem_storage.go
+++ internal/metric/mem_storage.go
@@ -1,6 +1,10 @@
 package metric
 
-import "errors"
+import (
+	"errors"
+	"math/rand"
+	"runtime"
+)
 
 type MemStorage struct {
 	gauge   map[string]float64
@@ -34,4 +38,46 @@ func (m *MemStorage) GetCounterMetric(name string) (int64, error) {
 	} else {
 		return 0, errors.New("no such metric")
 	}
+}
+func (m *MemStorage) CollectMetrics() {
+	var rtm runtime.MemStats
+	runtime.ReadMemStats(&rtm)
+
+	m.PutGaugeMetric("Alloc", float64(rtm.Alloc))
+	m.PutGaugeMetric("BuckHashSys", float64(rtm.BuckHashSys))
+	m.PutGaugeMetric("Frees", float64(rtm.Frees))
+	m.PutGaugeMetric("GCCPUFraction", float64(rtm.GCCPUFraction))
+	m.PutGaugeMetric("GCSys", float64(rtm.GCSys))
+	m.PutGaugeMetric("HeapAlloc", float64(rtm.HeapAlloc))
+	m.PutGaugeMetric("HeapIdle", float64(rtm.HeapIdle))
+	m.PutGaugeMetric("HeapInuse", float64(rtm.HeapInuse))
+	m.PutGaugeMetric("HeapObjects", float64(rtm.HeapObjects))
+	m.PutGaugeMetric("HeapReleased", float64(rtm.HeapReleased))
+	m.PutGaugeMetric("HeapSys", float64(rtm.HeapSys))
+	m.PutGaugeMetric("LastGC", float64(rtm.LastGC))
+	m.PutGaugeMetric("Lookups", float64(rtm.Lookups))
+	m.PutGaugeMetric("MCacheInuse", float64(rtm.MCacheInuse))
+	m.PutGaugeMetric("MCacheSys", float64(rtm.MCacheSys))
+	m.PutGaugeMetric("MSpanInuse", float64(rtm.MSpanInuse))
+	m.PutGaugeMetric("MSpanSys", float64(rtm.MSpanSys))
+	m.PutGaugeMetric("Mallocs", float64(rtm.Mallocs))
+	m.PutGaugeMetric("NextGC", float64(rtm.NextGC))
+	m.PutGaugeMetric("NumForcedGC", float64(rtm.NumForcedGC))
+	m.PutGaugeMetric("NumGC", float64(rtm.NumGC))
+	m.PutGaugeMetric("OtherSys", float64(rtm.OtherSys))
+	m.PutGaugeMetric("PauseTotalNs", float64(rtm.PauseTotalNs))
+	m.PutGaugeMetric("StackInuse", float64(rtm.StackInuse))
+	m.PutGaugeMetric("StackSys", float64(rtm.StackSys))
+	m.PutGaugeMetric("Sys", float64(rtm.Sys))
+	m.PutGaugeMetric("TotalAlloc", float64(rtm.TotalAlloc))
+	m.PutGaugeMetric("RandomValue", rand.Float64())
+	m.PutCounterMetric("PollCount", 1)
+}
+
+func (m *MemStorage) AllGaugeMetrics() map[string]float64 {
+	return m.gauge
+}
+
+func (m *MemStorage) AllCounterMetrics() map[string]int64 {
+	return m.counter
 }
