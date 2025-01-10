abd170264ee0ed927015a2627e43281b3fdd7217
Daria V. Khrustaleva <snowstorm1@yandex.ru>
2025-01-10 19:14:09 +0300

    Server refactoring.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,89 +1,21 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"strconv"
 
-	"github.com/dvkhr/metrix.git/internal/metric"
+	"github.com/dvkhr/metrix.git/internal/handlers"
 	"github.com/dvkhr/metrix.git/internal/storage"
 )
 
-var ms storage.MemStorage
-
-func gaugeMetric(res http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-	n := req.PathValue("name")
-	if len(n) == 0 {
-		http.Error(res, "Incorrect name!", http.StatusNotFound)
-		return
-	}
-
-	/*Debug output*/
-	sv, _ := ms.GetGaugeMetric(n)
-	fmt.Printf("BEFORE: Metric %s has value %g\n", n, sv)
-
-	v, err := strconv.ParseFloat(req.PathValue("value"), 64)
-	if err != nil {
-		http.Error(res, "Incorrect value!", http.StatusBadRequest)
-		return
-	}
-	ms.PutGaugeMetric(req.PathValue("name"), metric.GaugeMetricValue(v))
-
-	/*Debug output*/
-	sv, _ = ms.GetGaugeMetric(n)
-	fmt.Printf("AFTER: Metric %s has value %v\n", n, sv)
-	res.Write([]byte("Done!"))
-}
-
-func counterMetric(res http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
-		return
-	}
-	n := req.PathValue("name")
-
-	/*Debug output*/
-	sv, _ := ms.GetCounterMetric(n)
-	fmt.Printf("BEFORE: Metric %s has value %v\n", n, sv)
-
-	if len(n) == 0 {
-		http.Error(res, "Incorrect name!", http.StatusNotFound)
-		return
-	}
-	v, err := strconv.ParseInt(req.PathValue("value"), 10, 64)
-	if err != nil {
-		http.Error(res, "Incorrect value!", http.StatusBadRequest)
-		return
-	}
-	ms.PutCounterMetric(req.PathValue("name"), metric.CounterMetricValue(v))
-
-	/*Debug output*/
-	sv, _ = ms.GetCounterMetric(n)
-	fmt.Printf("AFTER: Metric %s has value %v\n", n, sv)
-	res.Write([]byte("Done!"))
-}
-
-func incorrectMetricRq(res http.ResponseWriter, req *http.Request) {
-	http.Error(res, "Incorrect update metric request!", http.StatusBadRequest)
-}
-
-func notfoundMetricRq(res http.ResponseWriter, req *http.Request) {
-	http.Error(res, "Metric not found!", http.StatusNotFound)
-}
-
 func main() {
-	ms.NewMemStorage()
+	MetricServer := handlers.NewMetricsServer(&storage.MemStorage{})
 
 	mux := http.NewServeMux()
-	mux.HandleFunc("/update/gauge/{name}/{value}", gaugeMetric)
-	mux.HandleFunc("/update/counter/{name}/{value}", counterMetric)
-	mux.HandleFunc("/update/gauge/", notfoundMetricRq)
-	mux.HandleFunc("/update/counter/", notfoundMetricRq)
-	mux.HandleFunc("/update/", incorrectMetricRq)
+	mux.HandleFunc("/update/gauge/{name}/{value}", MetricServer.HandlePutGaugeMetric)
+	mux.HandleFunc("/update/counter/{name}/{value}", MetricServer.HandlePutCounterMetric)
+	mux.HandleFunc("/update/gauge/", MetricServer.NotfoundMetricRq)
+	mux.HandleFunc("/update/counter/", MetricServer.NotfoundMetricRq)
+	mux.HandleFunc("/update/", MetricServer.IncorrectMetricRq)
 
 	err := http.ListenAndServe("localhost:8080", mux)
 	if err != nil {
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,91 @@
-package api
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/dvkhr/metrix.git/internal/metric"
+)
+
+type MetricStorage interface {
+	PutGaugeMetric(metricName string, metricValue metric.GaugeMetricValue) error
+	PutCounterMetric(metricName string, metricValue metric.CounterMetricValue) error
+	GetGaugeMetric(metricName string) (metric.GaugeMetricValue, error)
+	GetCounterMetric(metricName string) (metric.CounterMetricValue, error)
+
+	NewMemStorage()
+}
+
+type MetricsServer struct {
+	MetricStorage MetricStorage
+}
+
+func NewMetricsServer(MetricStorage MetricStorage) *MetricsServer {
+	MetricStorage.NewMemStorage()
+	return &MetricsServer{MetricStorage: MetricStorage}
+}
+
+func (ms *MetricsServer) HandlePutGaugeMetric(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	n := req.PathValue("name")
+	if len(n) == 0 {
+		http.Error(res, "Incorrect name!", http.StatusNotFound)
+		return
+	}
+
+	/*Debug output*/
+	sv, _ := ms.MetricStorage.GetGaugeMetric(n)
+	fmt.Printf("BEFORE: Metric %s has value %g\n", n, sv)
+
+	v, err := strconv.ParseFloat(req.PathValue("value"), 64)
+	if err != nil {
+		http.Error(res, "Incorrect value!", http.StatusBadRequest)
+		return
+	}
+	ms.MetricStorage.PutCounterMetric(req.PathValue("name"), metric.CounterMetricValue(v))
+
+	/*Debug output*/
+	sv, _ = ms.MetricStorage.GetGaugeMetric(n)
+	fmt.Printf("AFTER: Metric %s has value %v\n", n, sv)
+	res.Write([]byte("Done!"))
+}
+
+func (ms *MetricsServer) HandlePutCounterMetric(res http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(res, "Only POST requests are allowed!", http.StatusMethodNotAllowed)
+		return
+	}
+	n := req.PathValue("name")
+
+	/*Debug output*/
+	sv, _ := ms.MetricStorage.GetCounterMetric(n)
+	fmt.Printf("BEFORE: Metric %s has value %v\n", n, sv)
+
+	if len(n) == 0 {
+		http.Error(res, "Incorrect name!", http.StatusNotFound)
+		return
+	}
+	v, err := strconv.ParseInt(req.PathValue("value"), 10, 64)
+	if err != nil {
+		http.Error(res, "Incorrect value!", http.StatusBadRequest)
+		return
+	}
+	ms.MetricStorage.PutCounterMetric(req.PathValue("name"), metric.CounterMetricValue(v))
+
+	/*Debug output*/
+	sv, _ = ms.MetricStorage.GetCounterMetric(n)
+	fmt.Printf("AFTER: Metric %s has value %v\n", n, sv)
+	res.Write([]byte("Done!"))
+}
+
+func (ms *MetricsServer) IncorrectMetricRq(res http.ResponseWriter, req *http.Request) {
+	http.Error(res, "Incorrect update metric request!", http.StatusBadRequest)
+}
+
+func (ms *MetricsServer) NotfoundMetricRq(res http.ResponseWriter, req *http.Request) {
+	http.Error(res, "Metric not found!", http.StatusNotFound)
+}
