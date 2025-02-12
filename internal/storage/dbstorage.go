ef37c2c33d5f9040413429a0b126004172f29bce
Daria V. Khrustaleva <snowstorm1@yandex.ru>
2025-02-12 23:18:53 +0300

    Added db operations retry.

--- internal/storage/dbstorage.go
+++ internal/storage/dbstorage.go
@@ -27,37 +27,65 @@ func (ms *DBStorage) NewStorage() error {
 		return err
 	}
 
-	var createStmt = "create table if not exists metrix (id varchar(32) PRIMARY KEY, value jsonb not null)"
-	if _, err = ms.db.Exec(createStmt); err != nil {
-		var pgErr *pgconn.PgError
-		if errors.As(err, &pgErr) {
-			fmt.Printf("%v\n", pgErr.Code)
-		}
+	err = ms.retry(func() error {
+		err := ms.db.Ping()
+		return err
+	}, 3)
+	if err != nil {
 		return err
 	}
 
-	if ms.saveGaugeStmt, err = ms.db.Prepare("insert into metrix " +
-		"values($1::varchar, " +
-		"jsonb_build_object('id', $1::varchar, 'type', $2::varchar, 'value', $3::double precision)) " +
-		"on conflict(id) do update " +
-		"set value = jsonb_build_object('id', $1::varchar, 'type', $2::varchar, 'value', $3::double precision) " +
-		"where metrix.id = $1::varchar;"); err != nil {
+	var createStmt = "create table if not exists metrix (id varchar(32) PRIMARY KEY, value jsonb not null)"
+	err = ms.retry(func() error {
+		_, err = ms.db.Exec(createStmt)
+		return err
+	}, 3)
+	if err != nil {
 		return err
 	}
-	if ms.saveCounterStmt, err = ms.db.Prepare("insert into metrix " +
-		"values($1::varchar, " +
-		"jsonb_build_object('id', $1::varchar, 'type', $2::varchar, 'delta', $3::bigint)) " +
-		"on conflict(id) do update " +
-		"set value = jsonb_set(metrix.value, '{delta}', ((metrix.value ->> 'delta')::bigint + $3::bigint)::text::jsonb, false) " +
-		"where metrix.id = $1::varchar;"); err != nil {
+
+	err = ms.retry(func() error {
+		ms.saveGaugeStmt, err = ms.db.Prepare("insert into metrix " +
+			"values($1::varchar, " +
+			"jsonb_build_object('id', $1::varchar, 'type', $2::varchar, 'value', $3::double precision)) " +
+			"on conflict(id) do update " +
+			"set value = jsonb_build_object('id', $1::varchar, 'type', $2::varchar, 'value', $3::double precision) " +
+			"where metrix.id = $1::varchar;")
+		return err
+	}, 3)
+	if err != nil {
 		return err
 	}
-	if ms.getStmt, err = ms.db.Prepare("select value from metrix where id = $1::varchar;"); err != nil {
+
+	err = ms.retry(func() error {
+		ms.saveCounterStmt, err = ms.db.Prepare("insert into metrix " +
+			"values($1::varchar, " +
+			"jsonb_build_object('id', $1::varchar, 'type', $2::varchar, 'delta', $3::bigint)) " +
+			"on conflict(id) do update " +
+			"set value = jsonb_set(metrix.value, '{delta}', ((metrix.value ->> 'delta')::bigint + $3::bigint)::text::jsonb, false) " +
+			"where metrix.id = $1::varchar;")
+		return err
+	}, 3)
+	if err != nil {
 		return err
 	}
-	if ms.listStmt, err = ms.db.Prepare("select jsonb_object_agg(k,v) from metrix, jsonb_each(jsonb_build_object(id, value)) as t(k,v);"); err != nil {
+
+	err = ms.retry(func() error {
+		ms.getStmt, err = ms.db.Prepare("select value from metrix where id = $1::varchar;")
+		return err
+	}, 3)
+	if err != nil {
 		return err
 	}
+
+	err = ms.retry(func() error {
+		ms.listStmt, err = ms.db.Prepare("select jsonb_object_agg(k,v) from metrix, jsonb_each(jsonb_build_object(id, value)) as t(k,v);")
+		return err
+	}, 3)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -78,15 +106,19 @@ func (ms *DBStorage) retry(f func() error, maxRetries int) error {
 		if err == nil || !isPgTransportError(err) {
 			return err
 		}
-		fmt.Printf("%v\n", err)
+		fmt.Printf("Postgres retry after error %v\n", err)
 		time.Sleep(time.Duration(2*i+1) * time.Second)
 	}
 	return err
 }
 
 func (ms *DBStorage) Save(ctx context.Context, mt service.Metrics) error {
-	if ms.db.Ping() != nil {
-		return service.ErrUninitializedStorage
+	err := ms.retry(func() error {
+		err := ms.db.Ping()
+		return err
+	}, 3)
+	if err != nil {
+		return err
 	}
 
 	if len(mt.ID) == 0 {
@@ -109,26 +141,43 @@ func (ms *DBStorage) Save(ctx context.Context, mt service.Metrics) error {
 }
 
 func (ms *DBStorage) SaveAll(ctx context.Context, mt *[]service.Metrics) error {
-	if ms.db.Ping() != nil {
-		return service.ErrUninitializedStorage
+	err := ms.retry(func() error {
+		err := ms.db.Ping()
+		return err
+	}, 3)
+	if err != nil {
+		return err
 	}
 
 	if len(*mt) == 0 {
 		return service.ErrInvalidMetricName
 	}
 
-	pgTx, err := ms.db.BeginTx(ctx, nil)
+	var pgTx *sql.Tx
+
+	err = ms.retry(func() error {
+		pgTx, err = ms.db.BeginTx(ctx, nil)
+		return err
+	}, 3)
 	if err != nil {
 		return err
 	}
 	for _, metric := range *mt {
 		if metric.MType == service.GaugeMetric {
-			if _, err := ms.saveGaugeStmt.Exec(metric.ID, metric.MType, metric.Value); err != nil {
+			err = ms.retry(func() error {
+				_, err := ms.saveGaugeStmt.Exec(metric.ID, metric.MType, metric.Value)
+				return err
+			}, 3)
+			if err != nil {
 				pgTx.Rollback()
 				return err
 			}
 		} else if metric.MType == service.CounterMetric {
-			if _, err := ms.saveCounterStmt.Exec(metric.ID, metric.MType, metric.Delta); err != nil {
+			err = ms.retry(func() error {
+				_, err := ms.saveCounterStmt.Exec(metric.ID, metric.MType, metric.Delta)
+				return err
+			}, 3)
+			if err != nil {
 				pgTx.Rollback()
 				return err
 			}
@@ -137,14 +186,25 @@ func (ms *DBStorage) SaveAll(ctx context.Context, mt *[]service.Metrics) error {
 			return service.ErrInvalidMetricName
 		}
 	}
-	pgTx.Commit()
+
+	err = ms.retry(func() error {
+		pgTx.Commit()
+		return err
+	}, 3)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func (ms *DBStorage) Get(ctx context.Context, metricName string) (*service.Metrics, error) {
-	if ms.db.Ping() != nil {
-		return nil, service.ErrUninitializedStorage
+	err := ms.retry(func() error {
+		err := ms.db.Ping()
+		return err
+	}, 3)
+	if err != nil {
+		return nil, err
 	}
 
 	if len(metricName) == 0 {
@@ -153,7 +213,7 @@ func (ms *DBStorage) Get(ctx context.Context, metricName string) (*service.Metri
 
 	var data []byte
 	var mtrx service.Metrics
-	err := ms.retry(func() error {
+	err = ms.retry(func() error {
 		err := ms.getStmt.QueryRow(metricName).Scan(&data)
 		return err
 	}, 3)
@@ -169,13 +229,17 @@ func (ms *DBStorage) Get(ctx context.Context, metricName string) (*service.Metri
 }
 
 func (ms *DBStorage) List(ctx context.Context) (*map[string]service.Metrics, error) {
-	if ms.db.Ping() != nil {
-		return nil, service.ErrUninitializedStorage
+	err := ms.retry(func() error {
+		err := ms.db.Ping()
+		return err
+	}, 3)
+	if err != nil {
+		return nil, err
 	}
 
 	var data []byte
 	var mtrx map[string]service.Metrics
-	err := ms.retry(func() error {
+	err = ms.retry(func() error {
 		err := ms.listStmt.QueryRow().Scan(&data)
 		return err
 	}, 3)
@@ -210,14 +274,18 @@ func (ms *DBStorage) CheckStorage() error {
 }
 
 func (ms *DBStorage) ListSlice(ctx context.Context) ([]service.Metrics, error) {
-	if ms.db.Ping() != nil {
-		return nil, service.ErrUninitializedStorage
+	err := ms.retry(func() error {
+		err := ms.db.Ping()
+		return err
+	}, 3)
+	if err != nil {
+		return nil, err
 	}
 
 	var data []byte
 	var mtrx []service.Metrics
 
-	err := ms.retry(func() error {
+	err = ms.retry(func() error {
 		err := ms.listStmt.QueryRow().Scan(&data)
 		return err
 	}, 3)
