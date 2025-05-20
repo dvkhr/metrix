b0722de82c0b5a0b1d6b6cc55731709330e537e2
Daria V. Khrustaleva <snowstorm1@yandex.ru>
2025-05-20 23:56:47 +0300

    Разделила функции пакета crypto.

--- internal/crypto/keys.go
+++ internal/crypto/keys.go
@@ -1,13 +1,9 @@
 package crypto
 
 import (
-	"crypto/rand"
 	"crypto/rsa"
-	"crypto/sha256"
 	"crypto/x509"
-	"encoding/hex"
 	"encoding/pem"
-	"errors"
 	"fmt"
 	"os"
 )
@@ -35,28 +31,6 @@ func ReadPublicKey(filePath string) (*rsa.PublicKey, error) {
 	return pubKey, nil
 }
 
-// EncryptData шифрует данные с использованием публичного ключа.
-func EncryptData(data []byte, publicKey *rsa.PublicKey) (string, error) {
-	if publicKey == nil {
-		return "", fmt.Errorf("public key is nil")
-	}
-
-	// Шифрование данных с использованием OAEP
-	ciphertext, err := rsa.EncryptOAEP(
-		sha256.New(),
-		rand.Reader,
-		publicKey,
-		data,
-		nil,
-	)
-	if err != nil {
-		return "", fmt.Errorf("encryption failed: %w", err)
-	}
-
-	// Кодирование зашифрованных данных в Hex
-	return hex.EncodeToString(ciphertext), nil
-}
-
 // ReadPrivateKey читает и возвращает приватный ключ из файла.
 func ReadPrivateKey(filePath string) (*rsa.PrivateKey, error) {
 	keyBytes, err := os.ReadFile(filePath)
@@ -75,31 +49,4 @@ func ReadPrivateKey(filePath string) (*rsa.PrivateKey, error) {
 	}
 
 	return privKey, nil
-}
-
-// DecryptData расшифровывает данные с использованием приватного ключа.
-func DecryptData(encryptedData string, privateKey *rsa.PrivateKey) ([]byte, error) {
-	if privateKey == nil {
-		return nil, errors.New("private key is nil")
-	}
-
-	// Декодирование Hex
-	ciphertext, err := hex.DecodeString(encryptedData)
-	if err != nil {
-		return nil, fmt.Errorf("failed to decode hex data: %w", err)
-	}
-
-	// Расшифровка данных
-	plaintext, err := rsa.DecryptOAEP(
-		sha256.New(),
-		rand.Reader,
-		privateKey,
-		ciphertext,
-		nil,
-	)
-	if err != nil {
-		return nil, fmt.Errorf("decryption failed: %w", err)
-	}
-
-	return plaintext, nil
 }
