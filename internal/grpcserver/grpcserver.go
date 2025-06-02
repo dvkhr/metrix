05cb0e1f52ed0cf83149e9668e1fe7cd85ebe5b1
Daria V. Khrustaleva <snowstorm1@yandex.ru>
2025-06-02 17:04:07 +0300

    .

--- internal/grpcserver/grpcserver.go
+++ internal/grpcserver/grpcserver.go
@@ -1,1 +1,11 @@
 package grpcserver
+
+import (
+	pb "github.com/dvkhr/metrix.git/internal/grpc"
+	"github.com/dvkhr/metrix.git/internal/service"
+)
+
+type MetricsServer struct {
+	pb.UnimplementedMetricsServiceServer
+	MetricStorage service.MetricStorage
+}
